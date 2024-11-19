9fd33cd6a0135dc34fbc27c8e6e479eec5c8e425
Hojat <hojat72elect@gmail.com>
2024-11-19 02:40:33 -0500

    merged all the previous scripts into single code for GUI.

--- src/speak_out_the_text.py
+++ src/speak_out_the_text.py
@@ -9,13 +9,24 @@ def speak_out_the_text(input_text):
     # For Mac, If you face error related to "pyobjc" when running the `init()` method :
     # Install 9.0.1 version of pyobjc : "pip install pyobjc>=9.0.1"
 
+    rate = engine.getProperty('rate')
+    print(f"The rate of the voice engine is : {rate}")
+    # engine.setProperty('rate', 125) # this is for changing the rate of the engine
+
+    volume = engine.getProperty('volume')
+    print(f"The volume of the voice engine is :{volume}")
+    # engine.setProperty('volume', 1.0) # This is for changing the volume of the engine
+
+    # voices[0] is male and voices[1] is a female.
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+
     engine.say(input_text)
     engine.runAndWait()
 
 
 if __name__ == '__main__':
     import tkinter as tk
-    from tkinter import messagebox
 
     root = tk.Tk()
     root.title("Gooyesh text to Speech Engine")
